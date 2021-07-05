196afc4efa96e7a73bbb45911254286251fc71b4
Riccardo Orlando <orlandoricc@gmail.com>
2021-07-05 11:51:56 +0200

    Update dependencies.

--- setup.py
+++ setup.py
@@ -21,6 +21,6 @@ setuptools.setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    install_requires=["torch>=1.5,<1.10", "transformers>=4.3<4.6", "spacy>=3.0,<3.1"],
+    install_requires=["torch>=1.5,<1.10", "transformers>=4.3<4.9", "spacy>=3.0,<3.1"],
     python_requires=">=3.6",
 )
