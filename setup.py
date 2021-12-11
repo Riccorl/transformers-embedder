5db184d0388933e2d9aad4d6f0cfd6b5a3acbb6b
Riccardo Orlando <orlandoricc@gmail.com>
2021-12-11 16:14:22 +0100

    Fix import bug.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ install_requires = ["transformers>=4.3,<4.14"]
 
 setuptools.setup(
     name="transformers_embedder",  # Replace with your own username
-    version="1.8.3",
+    version="1.8.4",
     author="Riccardo Orlando",
     author_email="orlandoricc@gmail.com",
     description="Word level transformer based embeddings",
