ba0dbb5ba6c8dd0f8b4a1a0b5c7a2a824cdb0cae
Riccardo Orlando <orlandoricc@gmail.com>
2022-03-09 21:23:06 +0100

    Removed unused functions

--- transformers_embedder/utils.py
+++ transformers_embedder/utils.py
@@ -2,22 +2,11 @@ import importlib.util
 import logging
 
 _torch_available = importlib.util.find_spec("torch") is not None
-_spacy_available = importlib.util.find_spec("spacy") is not None
 
 
 def is_torch_available():
     """Check if PyTorch is available."""
     return _torch_available
-
-
-def is_spacy_available():
-    """Check if spaCy is available."""
-    return _spacy_available
-
-
-if is_torch_available():
-    import torch
-    from torch import Tensor
 
 
 def get_logger(name: str) -> logging.Logger:
